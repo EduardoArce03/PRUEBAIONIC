3c9166ab66ff07ebddb05ac563274c8ac24d6df1
pablo12b <pablobybravo@gmail.com>
2024-04-26 18:10:01 -0500

    nuevo
    nuevo
    
    Co-Authored-By: EduardoArce03 <107777936+EduardoArce03@users.noreply.github.com>

--- src/Vista/ExploreContainer.vue
+++ src/Vista/ExploreContainer.vue
@@ -4,67 +4,114 @@
       <ion-toolbar>
         <ion-title>Crear/Actualizar Persona</ion-title>
       </ion-toolbar>
-      <!-- Formulario para crear persona -->
-      <form @submit.prevent="submitForm">
-        <ion-item>
-          <ion-label position="floating">ID:</ion-label>
-          <ion-input @input="buscarPersona" v-model="id" type="text"></ion-input>
-        </ion-item>
-        <ion-item>
-          <ion-label position="floating">Nombre:</ion-label>
-          <ion-input v-model="nombre" type="text"></ion-input>
-        </ion-item>
-        <ion-button type="submit" expand="full">Crear Persona</ion-button>
-      </form>
+      <ion-button @click="setOpen(true)" expand="block" color="medium">Crear</ion-button>
+      <ion-modal :is-open="isOpen">
+        <ion-header>
+          <ion-toolbar>
+            <ion-buttons slot="start">
+              <ion-button @click="setOpen(false)">Cancel</ion-button>
+            </ion-buttons>
+            <ion-title>Crear</ion-title>
+            <ion-buttons slot="end">
+              <ion-button type="submit" @click="submitForm()">Confirm</ion-button>
+            </ion-buttons>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+          <form @submit.prevent="submitForm">
+            <ion-item>
+              <ion-label position="floating">ID:</ion-label>
+              <ion-input v-model="id" type="text"></ion-input>
+            </ion-item>
+            <ion-item>
+              <ion-label position="floating">Nombre:</ion-label>
+              <ion-input v-model="nombre" type="text"></ion-input>
+            </ion-item>
+          </form>
+        </ion-content>
+      </ion-modal>
       <!-- Mensaje de persona creada -->
-      <ion-card v-if="personaCreada || personaEncontrada">
+      <ion-card v-for="persona in personas" :key="persona.id">
         <ion-card-header>
-          <ion-card-title>Persona Encontrada</ion-card-title>
+          <ion-card-title>ID: {{ persona.id }}</ion-card-title>
         </ion-card-header>
-        <ion-card-content>
-          <ion-label>ID: {{ personaEncontrada.id }}
-            Nombre: {{ personaEncontrada.nombre }}</ion-label>
-        </ion-card-content>
-      </ion-card>
-      <ion-card v-if="personaCreada">
-        <ion-card-header>
-          <ion-card-title>Persona Creada</ion-card-title>
-        </ion-card-header>
-        <ion-card-content>
-          <ion-label>ID: {{ personaCreada.id }}
-            Nombre: {{ personaCreada.nombre }}</ion-label>
+        <ion-card-content class="ion-padding">
+          <ion-label>Nombre: {{ persona.nombre }}</ion-label>
+          <ion-fab slot="fixed" horizontal="end" vertical="bottom">
+            <ion-fab-button size="small" color="dark">
+              <ion-icon :icon="optionsSharp"></ion-icon>
+            </ion-fab-button>
+            <ion-fab-list side="start">
+              <ion-fab-button @click="submitDelete(persona.id)" color="danger">
+                <ion-icon :icon="trashSharp"></ion-icon>
+              </ion-fab-button>
+              <ion-fab-button id="open-modal" expand="block" @click="submitUpdate(persona.id, persona.nombre)"
+                color="warning">
+                <ion-icon :icon="pencilSharp"></ion-icon>
+              </ion-fab-button>
+            </ion-fab-list>
+          </ion-fab>
         </ion-card-content>
       </ion-card>
     </ion-content>
   </ion-page>
 </template>
 
-<script setup lang="ts">
-import { IonPage, IonContent, IonToolbar, IonTitle, IonCard, IonButton, IonItem, IonLabel, IonInput, IonCardContent, IonCardHeader, IonCardTitle, alertController } from '@ionic/vue';
-import { ref } from 'vue';
-import { insertarPersona, obtenerPersonaPorObjeto } from '@/Controlador/ControladorPersona';
+<script lang="ts" setup>
+import { IonModal, IonButtons, IonHeader, IonFabList, IonFabButton, IonFab, IonIcon, IonPage, IonContent, IonToolbar, IonTitle, IonCard, IonButton, IonItem, IonLabel, IonInput, IonCardContent, IonCardHeader, IonCardTitle, alertController } from '@ionic/vue';
+import { ref, onMounted } from 'vue';
+import { trashSharp, pencilSharp, optionsSharp } from 'ionicons/icons';
+import Persona from '@/Modelo/Persona'; // Importa la clase Persona
+import { insertarPersona, cargarPersonas, eliminarPersona } from '@/Controlador/ControladorPersona';
 
-const id = ref('');
-const nombre = ref('');
-const personaCreada = ref<any>(null);
+const id = ref<any>('');
+const nombre = ref<any>('');
 const personaEncontrada = ref<any>(null);
+const personas = ref<Persona[]>([]);
+const isOpen = ref(false);
 
-const buscarPersona = async () => {
+const setOpen = (open: boolean) => (isOpen.value = open);
+
+const submitDelete = async (id: number | undefined) => {
   try {
-    // Verificar que el ID no esté vacío
-    if (id.value !== '') {
-      console.log(id.value);
-      // Cargar la persona por su ID
-      const personaNue = await obtenerPersonaPorObjeto({ id: Number(id.value)});
-      // Asignar la persona encontrada a la variable personaEncontrada
-      personaEncontrada.value = personaNue.length > 0 ? personaNue[0] : null;
-    } else {
-      // Si el ID está vacío, asignar null a personaEncontrada
-      personaEncontrada.value = null;
-    }
+    const alert = await alertController.create({
+      header: 'Eliminar persona',
+      message: '¿Estás seguro de que deseas eliminar esta persona?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: () => {
+            console.log('Operación de eliminación cancelada');
+          }
+        },
+        {
+          text: 'Ok',
+          handler: async () => {
+            try {
+              await eliminarPersona({ id });
+              console.log('Éxito al eliminar persona');
+              reloadPageIfNeeded(); // Supongo que esta función recarga la página si es necesario
+            } catch (error) {
+              console.error('Error al eliminar persona:', error);
+              const alertError = await alertController.create({
+                header: 'Error al eliminar persona',
+                buttons: ['Ok'],
+              });
+              await alertError.present();
+            }
+          }
+        }
+      ],
+    });
+    await alert.present();
   } catch (error) {
-    console.error('Error al buscar persona:', error);
+    console.error('Error al mostrar la alerta:', error);
   }
+};
+
+const reloadPageIfNeeded = () => {
+  window.location.reload();
 };
 
 const submitForm = async () => {
@@ -73,7 +120,6 @@ const submitForm = async () => {
     id.value = '';
     nombre.value = '';
     console.log('Persona creada exitosamente:', nuevaPersona);
-    personaCreada.value = nuevaPersona;
     personaEncontrada.value = null;
     const alert = await alertController.create({
       header: 'Éxito al crear una nueva persona',
@@ -81,6 +127,7 @@ const submitForm = async () => {
         text: 'Ok',
       }],
     });
+    setOpen(false);
     await alert.present();
   } catch (error) {
     console.error('Error al crear persona:', error);
@@ -93,4 +140,18 @@ const submitForm = async () => {
     await alert.present();
   }
 };
+
+const submitUpdate = async (idN: number | undefined, nombreN: string | undefined) => {
+  try {
+    setOpen(true);
+    id.value = idN;
+    nombre.value = nombreN;
+  } catch (error) {
+    console.error('Error al crear persona:', error);
+  }
+};
+
+onMounted(async () => {
+  personas.value = await cargarPersonas();
+});
 </script>
